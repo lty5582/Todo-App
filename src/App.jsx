4103e39bbde9f91a4c864d722ba96f2fe7516d95
dwqcwda <lty5582@naver.com>
2024-12-23 19:12:57 +0900

    12/23일 도전과제

--- src/App.jsx
+++ src/App.jsx
@@ -3,84 +3,78 @@ import "./App.css";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
-  ]);
+    {id: 0, content: "밥먹기"},
+    {id: 1, content: "코딩 공부하기"},
+    {id: 2, content: "당근 거래하기"},
+  ])
 
-  return (
-    <>
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
-      <hr />
-      <TodoInput todoList={todoList} setTodoList={setTodoList} />
-    </>
-  );
+  return ( 
+  <>
+    <TodoList todoList={todoList}  setTodoList={setTodoList}/>
+    <hr/>
+    <TodoInput todoList={todoList} setTodoList={setTodoList}/>
+  </>
+  )
 }
 
-function TodoInput({ todoList, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
-
+function TodoInput({todoList, setTodoList}){  
+  const [inputvalue, setInputValue] = useState('')
   return (
     <>
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          const newTodo = { id: Number(new Date()), content: inputValue };
-          const newTodoList = [...todoList, newTodo];
-          setTodoList(newTodoList);
-          setInputValue("");
-        }}
-      >
-        추가하기
-      </button>
-    </>
-  );
-}
+    <input value={inputvalue} onChange={(event) =>
+      setInputValue(event.target.value)}/>
 
-function TodoList({ todoList, setTodoList }) {
+    <button onClick={() => {
+      const newTodo = { id: Number(new Date()), content: inputvalue}
+      const newTodoList =[...todoList, newTodo]
+      setTodoList(newTodoList)
+      setInputValue("")
+    }}
+    >
+      추가하기</button>
+    </>
+  )
+}
+  
+function TodoList({todoList, setTodoList}){
   return (
-    <ul>
-      {todoList.map((todo) => (
-        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
-      ))}
+      <ul>
+        {todoList.map((todo) => (
+          <Todo key={todo.id} todo={todo} setTodoList={setTodoList}/>  
+        ))}
     </ul>
-  );
+  )
 }
 
-function Todo({ todo, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
-  return (
-    <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
-    </li>
-  );
+function Todo({todo, setTodoList}) {
+  const [inputvalue, setInputValue] = useState('')
+  return ( 
+  <li>
+    {todo.content}
+    <input 
+    value={inputvalue}
+    onChange={(event) => setInputValue(event.target.value)}
+    />
+    <button onClick={() =>{
+      setTodoList(prev => {
+        return prev.map(el => {
+          if(el.id === todo.id){
+            return {...el, content: inputvalue}
+          }
+          return el
+        })
+      })
+    }}>수정</button>
+   <button
+    onClick={() => {
+      setTodoList(prev =>{
+        return prev.filter(el => el.id !== todo.id)
+    })
+   }}>
+    삭제
+    </button>
+  </li>
+  )
 }
-
-export default App;
+  
+export default App
